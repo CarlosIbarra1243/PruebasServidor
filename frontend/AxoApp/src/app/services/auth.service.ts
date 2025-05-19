c86dd3d7771c2f4c2eac8bc6ae966e8194571033
Isaac Luviano <isaacluvianohernandez@gmail.com>
2025-05-19 08:42:38 -0700

    fix: Modificación de la apiURL para realizar consultas desde el servicio de autenticación.

--- frontend/AxoApp/src/app/services/auth.service.ts
+++ frontend/AxoApp/src/app/services/auth.service.ts
@@ -9,7 +9,7 @@ import {jwtDecode} from 'jwt-decode';
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:3000/auth';
+  private apiUrl = '/auth';
 
   constructor(private http: HttpClient) {}
 

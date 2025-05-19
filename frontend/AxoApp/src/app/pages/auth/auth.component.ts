b08b29d2af608d2d92bfd846cedd58d0edf66629
Isaac Luviano <isaacluvianohernandez@gmail.com>
2025-05-19 08:57:25 -0700

    test: Modificación de componente de autenticación (función de fecha)

--- frontend/AxoApp/src/app/pages/auth/auth.component.ts
+++ frontend/AxoApp/src/app/pages/auth/auth.component.ts
@@ -98,7 +98,7 @@ export class AuthComponent {
   }
 
   private dateFormat(date: Date) : String {
-    const year = date.getFullYear;
+    const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
 
